039891e274723bb41df75cc537745d7096388630
satishraja631 <satishraja631@gmail.com>
2025-02-11 11:08:16 +0530

    returns the sum of numbers separated by comman

--- string-calculator/src/utils/stringCalculator.test.ts
+++ string-calculator/src/utils/stringCalculator.test.ts
@@ -7,3 +7,7 @@ test("return 0 for an empty string",()=>{
 test("returns the number itself for a single number string",()=>{
     expect(add("5")).toBe(5)
 })
+
+test("returns the sum of numbers separated by comma",()=>{
+    expect(add("1,2,4")).toBe(7);
+})
