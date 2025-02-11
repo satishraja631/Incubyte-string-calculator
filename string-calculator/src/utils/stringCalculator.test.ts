f4465534294cf0a42d0867617cde10320dbff96f
satishraja631 <satishraja631@gmail.com>
2025-02-11 11:10:27 +0530

    handles new line as delimitter

--- string-calculator/src/utils/stringCalculator.test.ts
+++ string-calculator/src/utils/stringCalculator.test.ts
@@ -11,3 +11,7 @@ test("returns the number itself for a single number string",()=>{
 test("returns the sum of numbers separated by comma",()=>{
     expect(add("1,2,4")).toBe(7);
 })
+
+test("handles new line as delimitter",()=>{
+    expect(add("1\n2,3")).toBe(6)
+})
